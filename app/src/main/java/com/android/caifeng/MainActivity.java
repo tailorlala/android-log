aa31da459c384beaa8005fc887037a76c47a4a19
caifeng <caifeng@caifengdeMacBook-Pro.local>
2017-02-16 15:43:26 +0800

    user define buildconfig field

--- app/src/main/java/com/android/caifeng/MainActivity.java
+++ app/src/main/java/com/android/caifeng/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("hello-world");
+        Log.d(BuildConfig.SERVER_HOST);
 
     }
 }
